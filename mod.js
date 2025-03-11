db93451ce221e519c05f746d6dd18a014655b6ed
Garden Gim <50333551+gardengim@users.noreply.github.com>
2025-03-11 23:13:27 +0900

    feat: command handling

--- mod.js
+++ mod.js
@@ -1,6 +1,5 @@
 //export stuffs
 
-
 //TODO:
 // [ ] command line arguments
 //   | commands:
@@ -33,64 +32,86 @@
 //   | then prevent running program even without build completed
 
 if (import.meta.main) {
-  const { parseArguments } = await import("./src/command.js");
+  const commandModule = await import("./src/command.js");
+  const { parseArguments } = commandModule;
   const { commands, options } = parseArguments(Deno.args);
+
+  // handle commands
+  // if command is not provided, show help
+  if (commands.length === 0) {
+    const { help } = commandModule;
+    help();
+  } // if command is provided, run the command
+  else {
+    // if command is related to project (check, prepare, build, run, clean... etc... ), we need to load configuration then
+    // `new` does not tho.
+    const command = commands[0];
+    if (command in commandModule) {
+      await commandModule[command](options);
+    } else {
+      console.error(`Unknown command: ${command}`);
+    }
+  }
 
   // configuration here?
   // load configuration
   // if configuration path is not provided, use default configuration path
+  //   order:
+  //     1. ./nativize.config.json
+  //       a. ./nativize.json
+  //       b. ./nativize.config.{android|ios|macos|linux|windows}.json
+  //     2. ./nativize.config.js
   // if configuration file is json/jsonc, whatever those are, parse it
   // if configuration file is js/ts, import it, then get default export
   //   this js/ts file should return configuration object
 
+  // let backendPath = options["--backend-path"];
+  //
+  // if (!backendPath) {
+  //   const { defaultBackend } = await import("./src/platform.js");
+  //   const { existsSync } = await import("@std/fs");
 
-  let backendPath = options["--backend-path"];
+  //   const backend = options["--backend"] ?? defaultBackend[Deno.build.os];
 
-  if (!backendPath) {
-    const { defaultBackend } = await import("./src/platform.js");
-    const { existsSync } = await import("@std/fs");
+  //   backendPath = `${Deno.cwd()}/.nativize/${backend}`;
 
-    const backend = options["--backend"] ?? defaultBackend[Deno.build.os];
+  //   if (!existsSync("./.nativize/")) {
+  //     await Deno.mkdir("./.nativize/");
+  //   }
 
-    backendPath = `${Deno.cwd()}/.nativize/${backend}`;
+  //   if (!existsSync(backendPath)) {
+  //     try {
+  //       await new Deno.Command("git", {
+  //         args: [
+  //           "clone",
+  //           `https://github.com/nativize/nativize-${backend}`,
+  //           backendPath,
+  //         ],
+  //       }).spawn().status;
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }
+  // }
 
-    if (!existsSync("./.nativize/")) {
-      await Deno.mkdir("./.nativize/");
-    }
+  // const { prepare, build, run, clean } = await import(
+  //   `file://${backendPath}/nativize.js`
+  // );
 
-    if (!existsSync(backendPath)) {
-      try {
-        await new Deno.Command("git", {
-          args: [
-            "clone",
-            `https://github.com/nativize/nativize-${backend}`,
-            backendPath,
-          ],
-        }).spawn().status;
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  }
-
-  const { prepare, build, run, clean } = await import(
-    `file://${backendPath}/nativize.js`
-  );
-
-  try {
-    await clean();
-    await prepare();
-    await build({
-      identifier: "com.nativize.abcde",
-      url: "https://naver.com",
-    });
-    await run({
-      identifier: "com.nativize.abcde",
-      avd: "Medium_Phone_API_35", //android specific
-    });
-  } catch (error) {
-    console.error(error);
-  } finally {
-    console.log("well... finally?");
-  }
+  // try {
+  //   await clean();
+  //   await prepare();
+  //   await build({
+  //     identifier: "com.nativize.abcde",
+  //     url: "https://naver.com",
+  //   });
+  //   await run({
+  //     identifier: "com.nativize.abcde",
+  //     avd: "Medium_Phone_API_35", //android specific
+  //   });
+  // } catch (error) {
+  //   console.error(error);
+  // } finally {
+  //   console.log("well... finally?");
+  // }
 }
